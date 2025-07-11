d3e92cd35a03cdc511e9807bf210c66c8524e446
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2025-07-11 11:16:22 +0100

    Order the character count list and add argument to analyse any book given

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import count_characters, get_words
+import sys
+
+from stats import count_characters, get_words, order_character_dict
 
 
 def get_book_text(path_to_file):
@@ -8,12 +10,24 @@ def get_book_text(path_to_file):
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text(sys.argv[1])
     word_count = get_words(book_text)
     character_dict = count_characters(book_text)
+    ordered_dict = order_character_dict(character_dict)
 
-    print(str(len(word_count)) + " words found in the document")
-    print(character_dict)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at " + sys.argv[1] + "...")
+    print("----------- Word Count ----------")
+    print("Found " + str(len(word_count)) + " total words")
+    print("--------- Character Count -------")
+    for char in ordered_dict:
+        if char["char"].isalpha():
+            print(char["char"] + ": " + str(char["num"]))
+    print("============= END ===============")
 
+
+if len(sys.argv) == 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,18 @@ def count_characters(book_text):
                 char_dict[letter] = 0
             char_dict[letter] += 1
     return char_dict
+
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(items):
+    return items["num"]
+
+
+def order_character_dict(char_dict):
+    ordered_dict = []
+
+    for char in char_dict:
+        ordered_dict.append({"char": char, "num": char_dict[char]})
+    ordered_dict.sort(reverse=True, key=sort_on)
+    return ordered_dict
